775c34fc54763cca6c4ba7762ec0b89fadb412ba
Kian Waters <S00187504@mail.itsligo.ie>
2023-08-28 14:45:40 +0100

    first commit

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-product-box',
+  templateUrl: './product-box.component.html',
+})
+export class ProductBoxComponent {
+  @Input() fullWidthMode = false;
+  @Input() product: Product | undefined;
+  @Output() addToCart = new EventEmitter();
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+    
+  }
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
+
+}
